140739e8b9fb0779457ceb0716fb7e1a40141570
K.L.T. Lakchani <cst21040@std.uwu.ac.lk>
2025-11-23 10:42:01 +0530

    updates

--- src/Pages/User/Home.jsx
+++ src/Pages/User/Home.jsx
@@ -1,9 +1,10 @@
 import Navbar from "../../Components/Header&Footer/Navbar.jsx";
+import AboutUs from "../../Components/Home/AboutUs.jsx";
 import Collection from "../../Components/Home/Collection.jsx";
 import HeroDetails from "../../Components/Home/HeroDetails.jsx";
 import HomeCarousel from "../../Components/Home/HomeCarousel.jsx";
 import NewArrivalCarousel from "../../Components/Home/NewArrivalCarousel.jsx";
-import OfferBanner from "./OfferBanner.jsx";
+import OfferBanner from "../../Components/Home/OfferBanner.jsx";
 function Home() {
     return (
         <>
@@ -13,6 +14,7 @@ function Home() {
             <NewArrivalCarousel/>
             <Collection/>
             <OfferBanner/>
+            <AboutUs/>
         </>
     )
 };
